9e507abc579d99aa18fae1f83614e87a3f4b9710
Rishabh Kumar <rishabhkumar1741@gamil.com>
2025-08-05 00:51:33 +0530

    first commit

---
+++ src/main/java/com/example/Week1Introduction/Week_1_/Introduction/Week1IntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Week1Introduction.Week_1_.Introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1IntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1IntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Week1Introduction/Week_1_/Introduction/Week1IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Week1Introduction.Week_1_.Introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
